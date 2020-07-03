9061a00808a3a1f622c6cb61c22401db650b9933
Rachele3 <r.falsiroli@studenti.unibs.it>
2020-07-03 14:46:20 +0200

    Classe per l'avvio del programma

---
+++ PgArEsame2020/src/it/unibs/ing/progettoarnaldo/esame2020/Main.java
@@ -0,0 +1,18 @@
+package it.unibs.ing.progettoarnaldo.esame2020;
+
+
+/**
+ * Avvio del programma
+ * @author Rachele
+ *
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
